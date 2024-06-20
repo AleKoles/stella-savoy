2ce1c07eaf23a102a068b19936f74c3ece1d00f7
Aleksandra Kolesnikova <52916334+AleKoles@users.noreply.github.com>
2024-06-20 20:01:11 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'theme-beige': '#f7f6f0',
+        'theme-blue': '#e9f4f5',
+        'theme-yellow': '#f3993f',
+        'theme-gray': '#304351',
+      },
+    },
+  },
+  plugins: [],
+};
