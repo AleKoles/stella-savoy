f62f2ace30bc67edd0e629b3deec5ee6c903a343
Aleksandra Kolesnikova <52916334+AleKoles@users.noreply.github.com>
2024-07-12 22:17:29 +0200

    add logo/ change navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import star from '../assets/star.png'
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import { Link } from 'react-scroll';
-
+import { TfiInstagram } from "react-icons/tfi";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -11,25 +12,26 @@ const Navbar = () => {
   };
 
   return (
-    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-theme-gray'>
-      <Link to="hero" spy={true} smooth={true} offset={50} duration={500}><h1 className='cursor-pointer w-full text-xl font-semibold text-theme-gray'>Stella Accounting</h1></Link>
-      <ul className='hidden md:flex'>
-        <li className='p-4'><Link to="about" spy={true} smooth={true} offset={50} duration={500}>About</Link></li>
-        <li className='p-4'>Resources</li>
-        <li className='p-4'>About</li>
-        <li className='p-4'>Contact</li>
-      </ul>
-      <div onClick={handleNav} className='block md:hidden'>
-          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
-      </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-theme-yellow m-4'>REACT.</h1>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'><Link>Company</Link></li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
-      </ul>
+    <div className='flex fixed top-0 z-50 bg-white/90 backdrop-blur-lg  w-full px-4 border-t-4 border-theme-blue1'>
+      <nav className="justify-between w-full flex items-center h-24 max-w-7xl mx-auto">  
+        <Link to="hero" spy={true} smooth={true} offset={50} duration={500} className="flex gap-2 flex-row items-center cursor-pointer"><img src={star} alt="spiritstone logo" className="h-8 w-8"/> <h1 className='w-full text-2xl font-semibold text-theme-gray cormorant flex items-center gap-1'>Stella Accounting </h1></Link>
+        <ul className='hidden md:flex items-center justify-end text-theme-gray2 gap-4 basis-1/3'>
+          <li className='group relative hover:text-black duration-150 ease-in-out tracking-wide cursor-pointer'><Link to="about" spy={true} smooth={true}  duration={500}>About</Link><span className="absolute bottom-0 left-0 w-0 group-hover:w-full border-t-2 border-theme-yellow transition-all duration-150"></span></li>
+          <li className='group relative hover:text-black duration-150 ease-in-out tracking-wide cursor-pointer'><Link to="gallery" spy={true} smooth={true} duration={500}>Works</Link><span className="absolute bottom-0 left-0 w-0 group-hover:w-full border-t-2 border-theme-yellow transition-all duration-150"></span></li>
+          <li className='group relative hover:text-black duration-150 ease-in-out tracking-wide cursor-pointer'><Link to="clients" spy={true} smooth={true}  duration={500}>Services</Link><span className="absolute bottom-0 left-0 w-0 group-hover:w-full border-t-2 border-theme-yellow transition-all duration-150"></span></li>
+          <li className='group relative hover:text-black duration-150 ease-in-out traking-wide cursor-pointer'><Link to="contact" spy={true} smooth={true}  duration={500}>Contact</Link><span className="absolute bottom-0 left-0 w-0 group-hover:w-full border-t-2 border-theme-yellow transition-all duration-150"></span></li>
+        </ul>
+        <div onClick={handleNav} className='block md:hidden z-50'>
+            {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+        </div>
+        <ul className={nav ? 'fixed left-0 top-24 w-full h-64 bg-white/90 backdrop-blur-lg flex flex-col justify-center items-center text-xl gap-4 py-12 px-4 ease-in-out duration-500' : 'ease-in-out duration-500 fixed top-24  bg-white/80 backdrop-blur-lg flex flex-col justify-center items-center text-lg gap-4 py-12 px-4 left-[-100%]'}>
+          <li className='group relative duration-150 ease-in-out tracking-wide cursor-pointer'><Link to="about" spy={true} smooth={true}  duration={500}>About</Link></li>
+          <li className='group relative duration-150 ease-in-out tracking-wide cursor-pointer'><Link to="gallery" spy={true} smooth={true} duration={500}>Works</Link></li>
+          <li className='group relative duration-150 ease-in-out tracking-wide cursor-pointer'><Link to="clients" spy={true} smooth={true}  duration={500}>Clients</Link></li>
+          <li className='group relative duration-150 ease-in-out traking-wide cursor-pointer'><Link to="contact" spy={true} smooth={true}  duration={500}>Contact</Link></li>
+        </ul>
+      </nav>
+     
     </div>
   );
 };
