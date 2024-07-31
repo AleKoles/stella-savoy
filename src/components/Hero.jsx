d4782a79c9941a815f1784519cad3bbf0db635c8
Aleksandra Kolesnikova <52916334+AleKoles@users.noreply.github.com>
2024-07-31 19:34:00 +0200

    optimize images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,9 +8,13 @@ import { Link } from 'react-scroll';
 const Hero = () => {
   return (
     <div className='text-theme-gray mt-24 lg:mb-12 lg:mx-8 2xl:mx-0' id="hero">
-      <div className=' max-w-7xl w-full h-[90vh] mx-auto text-center grid gap-16 grid-cols-1 lg:grid-cols-2'>
+      <div className=' max-w-7xl w-full h-[70vh] lg:h-[90vh] mx-auto text-center grid gap-16 grid-cols-1 lg:grid-cols-2'>
         <div className="relative bg-theme-gray/80 lg:bg-transparent flex flex-col gap-8 h-full w-full items-center justify-center px-4">
-        <img className='top-0 -z-10  absolute object-cover lg:hidden w-full h-full' src={hero} alt="/" />
+        <img
+            className='top-0 -z-10 absolute object-cover lg:hidden w-full h-full'
+            src={hero}
+            alt="Hero"
+          />
           <h1 className='text-theme-yellow  xl:text-7xl md:text-6xl sm:text-5xl text-4xl font-bold'>
           Довірте свої податки професіоналам
           </h1>
